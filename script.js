b433b99a7057113610a58054321666e23a03e318
thaNorth$tar <114375239+thaNorthStar@users.noreply.github.com>
2022-11-03 23:28:29 -0500

    main code
    main code everything is done except some styling & features to improve user experience

--- script.js
+++ script.js
@@ -1,57 +1,58 @@
-const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by-popularity.desc&api_key=77eaca018384060453847fe9dc474989&page=1';
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=77eaca018384060453847fe9dc474989&query=";
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&page=1';
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&query=";
+
 
 const main = document.getElementById("section");
 const form = document.getElementById("form");
 const search = document.getElementById("query");
 
 returnMovies(APILINK)
-function returnMovies(url){
+function returnMovies(url) {
   fetch(url).then(res => res.json())
-  .then(function(data){
-  console.log(data.results);
-  data.results.forEach(element => {
-      const div_card = document.createElement('div');
-      div_card.setAttribute('class', 'card');
-      
-      const div_row = document.createElement('div');
-      div_row.setAttribute('class', 'row');
-      
-      const div_column = document.createElement('div');
-      div_column.setAttribute('class', 'column');
-      
-      const image = document.createElement('img');
-      image.setAttribute('class', 'thumbnail');
-      image.setAttribute('id', 'image');
-      
-      const title = document.createElement('h3');
-      title.setAttribute('id', 'title');
-      
-      const center = document.createElement('center');
+    .then(function(data) {
+      console.log(data.results);
+      data.results.forEach(element => {
+        const div_card = document.createElement('div');
+        div_card.setAttribute('class', 'card');
 
-      title.innerHTML = `${element.title}`;
-      image.src = IMG_PATH + element.poster_path;
+        const div_row = document.createElement('div');
+        div_row.setAttribute('class', 'row');
 
-      center.appendChild(image);
-      div_card.appendChild(center);
-      div_card.appendChild(title);
-      div_column.appendChild(div_card);
-      div_row.appendChild(div_column);
+        const div_column = document.createElement('div');
+        div_column.setAttribute('class', 'column');
 
-      main.appendChild(div_row);
-  });
-});
+        const image = document.createElement('img');
+        image.setAttribute('class', 'thumbnail');
+        image.setAttribute('id', 'image');
+
+        const title = document.createElement('h3');
+        title.setAttribute('id', 'title');
+
+        const center = document.createElement('center');
+
+        title.innerHTML = `${element.title}<br><a href="movie.html?id=${element.id}&title=${element.title}">reviews</a>`;
+        image.src = IMG_PATH + element.poster_path;
+
+        center.appendChild(image);
+        div_card.appendChild(center);
+        div_card.appendChild(title);
+        div_column.appendChild(div_card);
+        div_row.appendChild(div_column);
+
+        main.appendChild(div_row);
+      });
+    });
 }
 
 form.addEventListener("submit", (e) => {
-e.preventDefault();
-main.innerHTML = '';
+  e.preventDefault();
+  main.innerHTML = '';
 
-const searchItem = search.value;
+  const searchItem = search.value;
 
-if (searchItem) {
-  returnMovies(SEARCHAPI + searchItem);
+  if (searchItem) {
+    returnMovies(SEARCHAPI + searchItem);
     search.value = "";
-}
+  }
 });
